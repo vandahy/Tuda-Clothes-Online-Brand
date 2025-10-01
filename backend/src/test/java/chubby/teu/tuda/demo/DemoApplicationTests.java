6e74339eadce5fdd70fbd9e8cf587b9d629a5328
Van Dahy <vandahy@gmail.com>
2025-10-01 10:50:00 +0700

    First commit

---
+++ backend/src/test/java/chubby/teu/tuda/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package chubby.teu.tuda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
