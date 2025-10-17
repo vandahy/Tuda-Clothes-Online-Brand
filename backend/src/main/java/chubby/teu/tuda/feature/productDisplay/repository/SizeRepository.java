99c7b32ef0acd7bebd0bb0f05db67b7320ed3aef
Van Dahy <vandahy@gmail.com>
2025-10-17 22:56:56 +0700

    edit annotation in repository backend

--- backend/src/main/java/chubby/teu/tuda/feature/productDisplay/repository/SizeRepository.java
+++ backend/src/main/java/chubby/teu/tuda/feature/productDisplay/repository/SizeRepository.java
@@ -2,11 +2,12 @@ package chubby.teu.tuda.feature.productDisplay.repository;
 
 import chubby.teu.tuda.core.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
+@Repository
 public interface SizeRepository extends JpaRepository<Size, Integer> {
     Optional<Size> findByName(String name);
 }
