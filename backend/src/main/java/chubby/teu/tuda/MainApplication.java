4a12d7fc25935fb59a540aca3dd2e51516d2b655
Van Dahy <vandahy@gmail.com>
2025-10-22 21:37:45 +0700

    sửa application.properties, User

--- backend/src/main/java/chubby/teu/tuda/MainApplication.java
+++ backend/src/main/java/chubby/teu/tuda/MainApplication.java
@@ -1,13 +1,15 @@
 package chubby.teu.tuda;
 
+
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 
 }
